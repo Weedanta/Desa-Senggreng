a5c60cb1203839a453f1e00041c6055f88531ad2
Weedanta <baguswedanta@student.ub.ac.id>
2025-07-23 19:12:52 +0700

    feat: add wisata page

---
+++ src/app/wisata/layout.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/shared/components/layout/footer/footer";
+import { NavbarContainer } from "@/shared/components/layout/navbar/container";
+
+export default function layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-50">
+      <NavbarContainer />
+      {children}
+      <Footer />
+    </div>
+  );
+}
