4a5ebef3b851da637a63a88161c3032e706da111
wedanta <baguswedanta17@gmail.com>
2025-07-24 14:25:37 +0700

    fix: fix bug

--- src/shared/components/layout/navbar/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as NavbarContainer } from './container';
-export { SimpleNavbarContainer } from './container';
-export * from './components';
-export * from './hooks';
-export * from './types';
